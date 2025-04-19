0fae6e4b25caeb13feb08017fe017b62a721ce05
lukatonny <ferreira16092007@gmail.com>
2025-04-19 00:32:23 -0300

    icon adicionado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" type="image/png" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} bg-background`}>
         {children}
       </body>
